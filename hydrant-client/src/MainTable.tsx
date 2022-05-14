677cfa0a20eb36d5dfe5afe095dd82497812257e
Robert Cunningham <robertvcunningham@gmail.com>
2022-05-14 18:58:56 -0400

    add some reviews

--- hydrant-client/src/MainTable.tsx
+++ hydrant-client/src/MainTable.tsx
@@ -1,7 +1,7 @@
 import MaterialTable from "@material-table/core"
 import { Paper } from "@material-ui/core"
 import _ from "lodash"
-import d from "./test_data.json"
+import d from "./data.json"
 
 type CourseInfo = {
   course_number: string
@@ -9,9 +9,7 @@ type CourseInfo = {
 }
 
 export const MainTable = ({ search }: { search: string }) => {
-  const headerStyle = { background: "inherit" }
-
-  const filtered = _(d)
+  const filtered = _(d as CourseInfo[])
     .filter((course) =>
       JSON.stringify(course.course_name + course.course_number)
         .toLowerCase()
@@ -26,12 +24,7 @@ export const MainTable = ({ search }: { search: string }) => {
           {
             title: "Course",
             sorting: false,
-            render: (p: CourseInfo) => (
-              <div className="grid w-5/8 bg-slate-100">
-                <p className="text-slate-700">{p.course_number}</p>
-                <p className="text-slate-400 text-sm truncate">{p.course_name}</p>
-              </div>
-            ),
+            render: (c: CourseInfo) => <TitleCell course={c}></TitleCell>,
             cellStyle: { width: "100vw" },
           },
           {
@@ -54,7 +47,7 @@ export const MainTable = ({ search }: { search: string }) => {
           pageSize: 10,
           pageSizeOptions: [],
           showFirstLastPageButtons: false,
-          headerStyle,
+          headerStyle: { background: "inherit" },
           detailPanelType: "single",
           //tableLayout: "fixed",
         }}
@@ -62,3 +55,10 @@ export const MainTable = ({ search }: { search: string }) => {
     </div>
   )
 }
+
+const TitleCell = ({ course }: { course: CourseInfo }) => (
+  <div className="grid w-5/8 bg-slate-100">
+    <p className="text-slate-700">{course.course_number}</p>
+    <p className="text-slate-400 text-sm truncate">{course.course_name}</p>
+  </div>
+)
