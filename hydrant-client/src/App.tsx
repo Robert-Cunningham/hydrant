07b461e91aa24cd6fc0d386fa4267709e189c418
Robert Cunningham <robertvcunningham@gmail.com>
2022-05-13 23:52:24 -0400

    fix hyrant -> hydrant

---
+++ hydrant-client/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { Button, ChakraProvider } from "@chakra-ui/react"
+
+const App = () => {
+  return <Button>test button</Button>
+}
+
+export default App
