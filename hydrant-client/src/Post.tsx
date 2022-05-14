327cab536be1d334a31b15b2efcc388948580997
Robert Cunningham <robertvcunningham@gmail.com>
2022-05-14 16:51:55 -0400

    cleaning

--- hydrant-client/src/Post.tsx
+++ hydrant-client/src/Post.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { useParams } from "react-router-dom"
-import Bestpost from "../posts/hassa.mdx"
+import HASSAPost from "../posts/hassa.mdx"
 
 export const Post = () => {
   const { id } = useParams<{ id: string }>()
 
   return (
-    <article className="prose">
-      <Bestpost></Bestpost>
+    <article className="prose p-8">
+      <HASSAPost></HASSAPost>
     </article>
   )
 }
