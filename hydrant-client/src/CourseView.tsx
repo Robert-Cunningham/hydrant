8b5135ac97e8a4a0d9c035c4e2db58dda0f633b0
Kosi Nwabueze <kosinwabueze@gmail.com>
2022-05-16 16:12:56 -0400

    added dropdown for graphs

--- hydrant-client/src/CourseView.tsx
+++ hydrant-client/src/CourseView.tsx
@@ -1,6 +1,14 @@
 import { CourseTerm, FullCourseData, RatingType } from "./data"
-import { VStack, HStack, Text } from "@chakra-ui/react"
-import { LineChart, LineSeries, PointSeries, TooltipArea, ChartTooltip } from "reaviz"
+import { VStack, HStack, Text, Select, Box } from "@chakra-ui/react"
+import {
+  LineChart,
+  LineSeries,
+  PointSeries,
+  TooltipArea,
+  ChartTooltip,
+  PieChart,
+  PieArcSeries,
+} from "reaviz"
 import _ from "lodash"
 import React from "react"
 
@@ -29,6 +37,13 @@ const monthNames = [
   "November",
   "December",
 ]
+
+type ChartInfoEntry = {
+  text: string
+  component: JSX.Element
+}
+
+type ChartInfoTable = Record<string, ChartInfoEntry>
 
 function htmlDecode(input: string): string {
   const e = document.createElement("div")
@@ -74,8 +89,124 @@ export const CourseView = ({ course }: { course: FullCourseData }) => {
 
   const { firehose } = course
 
+  const [chart, setChart] = React.useState<string | undefined>(undefined)
+
   if (!firehose) {
     return <React.Fragment />
+  }
+
+  const chartInfo: ChartInfoTable = {
+    hours: {
+      text: `Hours spent inside vs. outside of class`,
+      component: (
+        <React.Fragment>
+          <PieChart
+            // width={300}
+            // height={350}
+            data={[
+              { key: "Inside", data: course.computed.inclassHours },
+              { key: "Outside", data: course.computed.outclassHours },
+            ]}
+            series={
+              <PieArcSeries
+                cornerRadius={4}
+                padAngle={0.02}
+                padRadius={200}
+                doughnut={true}
+                colorScheme="Set2"
+              />
+            }
+          />
+          <Text fontWeight="normal" fontSize="xs">
+            Avg. hours spent inside vs. outside class
+          </Text>
+        </React.Fragment>
+      ),
+    },
+    enrollment: {
+      text: `Enrollment since ${enrollmentHistorical[0].key.getFullYear().toString()}`,
+      component: (
+        <React.Fragment>
+          <LineChart
+            // width={300}
+            // height={350}
+            series={
+              <LineSeries
+                tooltip={
+                  <TooltipArea
+                    tooltip={
+                      <ChartTooltip
+                        followCursor={true}
+                        modifiers={{
+                          offset: "5px, 50px",
+                        }}
+                        content={(d: any) => (
+                          <VStack spacing={2}>
+                            <Text fontSize="xs">
+                              {monthNames[d.key.getMonth()]} {d.key.getFullYear()}
+                            </Text>
+                            <Text fontSize="xs">{d.value}</Text>
+                            <Text fontSize="xs">{d.metadata}</Text>
+                          </VStack>
+                        )}
+                      />
+                    }
+                  />
+                }
+                interpolation="smooth"
+                symbols={<PointSeries show={true} />}
+              />
+            }
+            data={enrollmentHistorical}
+          />
+          <Text fontWeight="normal" fontSize="xs">
+            Enrollment since {enrollmentHistorical[0].key.getFullYear().toString()}
+          </Text>
+        </React.Fragment>
+      ),
+    },
+    rating: {
+      text: `Course rating since ${ratingHistorical[0].key.getFullYear().toString()}`,
+      component: (
+        <React.Fragment>
+          <LineChart
+            // width={300}
+            // height={350}
+            series={
+              <LineSeries
+                tooltip={
+                  <TooltipArea
+                    tooltip={
+                      <ChartTooltip
+                        followCursor={true}
+                        modifiers={{
+                          offset: "5px, 50px",
+                        }}
+                        content={(d: any) => (
+                          <VStack spacing={2}>
+                            <Text fontSize="xs">
+                              {monthNames[d.key.getMonth()]} {d.key.getFullYear()}
+                            </Text>
+                            <Text fontSize="xs">{d.value}</Text>
+                            <Text fontSize="xs">{d.metadata}</Text>
+                          </VStack>
+                        )}
+                      />
+                    }
+                  />
+                }
+                interpolation="smooth"
+                symbols={<PointSeries show={true} />}
+              />
+            }
+            data={ratingHistorical}
+          />
+          <Text fontWeight="normal" fontSize="xs">
+            Avg. course rating since {ratingHistorical[0].key.getFullYear().toString()}
+          </Text>
+        </React.Fragment>
+      ),
+    },
   }
 
   return (
@@ -88,7 +219,7 @@ export const CourseView = ({ course }: { course: FullCourseData }) => {
       marginX="auto"
     >
       <VStack marginTop={2} spacing={8} align="center" width="100%">
-        <HStack spacing={10} width="100%" align="flex-start">
+        <HStack spacing={6} width="100%" align="flex-start">
           <Text fontSize="sm">
             <Text display="inline" fontWeight="bold">
               Prereqs
@@ -111,109 +242,41 @@ export const CourseView = ({ course }: { course: FullCourseData }) => {
           ) : (
             <React.Fragment />
           )}
+          <Text fontSize="sm">
+            <Text display="inline" fontWeight="bold">
+              Units
+            </Text>
+            : {firehose.u1 + firehose.u2 + firehose.u3}
+          </Text>
         </HStack>
-        <VStack align="flex-start">
+        <VStack width="100%" align="flex-start">
           <Text fontWeight="bold" fontSize="md">
             Summary
           </Text>
           <Text fontSize="sm" dangerouslySetInnerHTML={{ __html: htmlDecode(firehose.d) }} />
         </VStack>
-        <HStack spacing={12}>
-          <VStack>
-            {/* <PieChart
-              width={400}
-              height={350}
-              data={[
-                { key: "Inside", data: course.computed.inclassHours },
-                { key: "Outside", data: course.computed.outclassHours },
-              ]}
-              series={
-                <PieArcSeries
-                  cornerRadius={4}
-                  padAngle={0.02}
-                  padRadius={200}
-                  doughnut={true}
-                  colorScheme="Set2"
-                />
-              }
-            />
-            <Text fontWeight="normal" fontSize="xs">
-              Avg. hours spent inside vs. outside class
-            </Text> */}
-            <LineChart
-              width={400}
-              height={350}
-              series={
-                <LineSeries
-                  tooltip={
-                    <TooltipArea
-                      tooltip={
-                        <ChartTooltip
-                          followCursor={true}
-                          modifiers={{
-                            offset: "5px, 50px",
-                          }}
-                          content={(d: any) => (
-                            <VStack spacing={2}>
-                              <Text fontSize="xs">
-                                {monthNames[d.key.getMonth()]} {d.key.getFullYear()}
-                              </Text>
-                              <Text fontSize="xs">{d.value}</Text>
-                              <Text fontSize="xs">{d.metadata}</Text>
-                            </VStack>
-                          )}
-                        />
-                      }
-                    />
-                  }
-                  interpolation="smooth"
-                  symbols={<PointSeries show={true} />}
-                />
-              }
-              data={enrollmentHistorical}
-            />
-            <Text fontWeight="normal" fontSize="xs">
-              Avg. enrollment since {enrollmentHistorical[0].key.getFullYear().toString()}
-            </Text>
+        <Select
+          value={chart}
+          onChange={(e) => setChart(e.target.value)}
+          colorScheme="messenger"
+          size="md"
+          variant="flushed"
+          placeholder="Chart"
+        >
+          {_.keys(chartInfo).map((chart) => (
+            <option key={chart} value={chart}>
+              {_.get(chartInfo, chart).text}
+            </option>
+          ))}
+          {/* <option value="hours">Hours spent inside vs. outside of class</option>
+          <option value="hours">Enrollment since {enrollmentHistorical[0].key.getFullYear().toString()}</option>
+          <option value='hours'>Course rating since {ratingHistorical[0].key.getFullYear().toString()}</option> */}
+        </Select>
+        {chart !== undefined && chartInfo.hasOwnProperty(chart) && (
+          <VStack width="40vw" height="50vh" marginX="auto" spacing={6}>
+            {chartInfo[chart].component}
           </VStack>
-          <VStack>
-            <LineChart
-              width={400}
-              height={350}
-              series={
-                <LineSeries
-                  tooltip={
-                    <TooltipArea
-                      tooltip={
-                        <ChartTooltip
-                          followCursor={true}
-                          modifiers={{
-                            offset: "5px, 50px",
-                          }}
-                          content={(d: any) => (
-                            <VStack spacing={2}>
-                              <Text fontSize="xs">
-                                {monthNames[d.key.getMonth()]} {d.key.getFullYear()}
-                              </Text>
-                              <Text fontSize="xs">{d.value}</Text>
-                              <Text fontSize="xs">{d.metadata}</Text>
-                            </VStack>
-                          )}
-                        />
-                      }
-                    />
-                  }
-                  interpolation="smooth"
-                  symbols={<PointSeries show={true} />}
-                />
-              }
-              data={ratingHistorical}
-            />
-            <Text fontWeight="normal" fontSize="xs">
-              Avg. course rating since {ratingHistorical[0].key.getFullYear().toString()}
-            </Text>
-          </VStack>
-        </HStack>
+        )}
       </VStack>
     </VStack>
   )
